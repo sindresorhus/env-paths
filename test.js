5db8484959d2642ef23cae64c92699a4e7b6bef1
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-07-01 12:14:27 +0200

    Add tests for suffixes

--- test.js
+++ test.js
@@ -1,13 +1,46 @@
 import test from 'ava';
 import m from './';
 
-test(t => {
+test('default suffix', t => {
 	const name = 'unicorn';
 	const paths = m(name);
 
+	console.log(`name: '${name}' suffix = undefined`);
+
 	Object.keys(paths).forEach(key => {
 		const val = paths[key];
-		console.log(`${key}: ${val}`);
-		t.true(val.includes(name));
+		console.log(`  ${key}: ${val}`);
+		t.true(val.endsWith(`${name}-nodejs`));
+	});
+
+	console.log('\n');
+});
+
+test('custom suffix', t => {
+	const name = 'unicorn';
+	const suffix = 'horn';
+	const paths = m(name, suffix);
+
+	console.log(`name: '${name}' suffix = '${suffix}'`);
+
+	Object.keys(paths).forEach(key => {
+		const val = paths[key];
+		console.log(`  ${key}: ${val}`);
+		t.true(val.endsWith(`${name}-${suffix}`));
+	});
+
+	console.log('\n');
+});
+
+test('no suffix', t => {
+	const name = 'unicorn';
+	const paths = m(name, null);
+
+	console.log(`name: '${name}' suffix = null`);
+
+	Object.keys(paths).forEach(key => {
+		const val = paths[key];
+		console.log(`  ${key}: ${val}`);
+		t.true(val.endsWith(`${name}`));
 	});
 });
