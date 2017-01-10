3a2ba84dc8be3103158225b4f0a3bd36ba9288b6
Oden <ops991923@gmail.com>
2017-01-09 19:55:45 -0800

    Fix incorrect paths on Linux (#6)

--- test.js
+++ test.js
@@ -25,3 +25,27 @@ test('no suffix', t => {
 	const paths = m(name, opts);
 	t.true(paths.data.endsWith(name));
 });
+
+// linux-specific tests
+if (process.platform === 'linux') {
+	test('correct paths with XDG_*_HOME set', t => {
+		const envVars = {
+			data: 'XDG_DATA_HOME',
+			config: 'XDG_CONFIG_HOME',
+			cache: 'XDG_CACHE_HOME',
+			log: 'XDG_STATE_HOME'
+		};
+
+		Object.values(envVars).forEach(env => {
+			process.env[env] = `/tmp/${env}`;
+		});
+
+		const name = 'unicorn';
+		const paths = m(name);
+
+		Object.keys(envVars).forEach(env => {
+			const expectedPath = process.env[envVars[env]];
+			t.true(paths[env].startsWith(expectedPath) && paths[env].endsWith(`${name}-nodejs`));
+		});
+	});
+}
